662b02ed3b2f44e3695456058a245018f669e836
Michael Aklilu <michaelaklilu@gmail.com>
2024-12-18 09:30:26 +0300

    Implemented logic for login in frontend

--- frontend/recipesFrontend/src/components/Login.jsx
+++ frontend/recipesFrontend/src/components/Login.jsx
@@ -1,21 +1,51 @@
-
+import { useState } from 'react'
+import  loginService from '../services/login' 
 const Login = () => {
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    try{
+      loginService.login({username,password})
+      setUsername('')
+      setPassword('')
+      event.target.username.value = ''
+      event.target.password.value = ''
+    }catch{
+      console.log('error');
+    }
+   
+  }
+  
+  const handleUsername = (event) => {
+    setUsername(event.target.value)
+    
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+  
     return (
      <div className="flex justify-center items-center h-screen bg-login-bg bg-cover bg-center">
         <div className="w-96 p-6 shadow-lg bg-gray-700 rounded-md ">
+          <form onSubmit={handleSubmit}>
           <h1 className="text-3xl block text-center font-semibold text-white "><i className="fa-solid fa-user text-white"></i> Login</h1>
           <hr className="mt-3" />
           <div className="mt-3">
             <label htmlFor="username" className="block text-base mb-2 text-white">Username</label>
-            <input type="text" id="username" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Username..." />
+            <input type="text" name='username' id="username" onChange={handleUsername} className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Username..." />
           </div>
           <div className="mt-3">
             <label htmlFor="password" className="block text-base mb-2 text-white">Password</label>
-            <input type="password" id="password" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Password..." />
+            <input type="password" name='password' onChange={handlePassword}  id="password" className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Password..." />
           </div>
           <div className="mt-5 flex justify-center">
             <button type="submit"className="border-2 border-white-600  bg-white text-gray-700 py-1 px-4 rounded-md">Login</button>
           </div>
+          </form>
+          
         </div>  
      </div>
   )
--- frontend/recipesFrontend/src/services/login.js
+++ frontend/recipesFrontend/src/services/login.js
@@ -1,2 +1,10 @@
+import axios from 'axios'
+const url = 'http://localhost:3000/api/login'
+
+const login = async (credentials) =>{
+    const response = await axios.post(url,credentials)  
+    return response.data
+}
 
 
+export default { login } 
